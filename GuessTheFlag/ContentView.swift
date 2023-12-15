8b2c65b3007a1f8304ca66d6f2b28c2809eb1555
Daniel Yankiver <dyankiver@gmail.com>
2023-12-15 15:43:17 -0500

    showing score with alert

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-  var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK",  "Ukraine", "US" ].shuffled()
-  var correctAnswer = Int.random(in: 0...2)
+  @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK",  "Ukraine", "US" ].shuffled()
+  @State private var correctAnswer = Int.random(in: 0...2)
 
   @State private var showingScore = false
   @State private var scoreTitle = ""
@@ -37,6 +37,26 @@ struct ContentView: View {
         }
       }
     }
+    .alert(scoreTitle, isPresented: $showingScore) {
+      Button("Continue", action: askQuestion)
+    } message: {
+      Text("Your score is ???")
+    }
+  }
+
+  func flagTapped(_ number: Int) {
+    if number == correctAnswer {
+      scoreTitle = "Correct"
+    } else {
+      scoreTitle = "Wrong"
+    }
+
+    showingScore = true
+  }
+
+  func askQuestion() {
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0...2)
   }
 }
 
